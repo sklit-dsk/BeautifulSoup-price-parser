ba6ecbb9a57b56b80bc6a9380358eba62f9e26f5
sklDanila <danila.sklovskii@gmail.com>
2024-03-27 08:38:24 +0100

    new commit

--- trainingfile.py
+++ trainingfile.py
@@ -14,10 +14,16 @@ def parse_page(url):
         # Находим все элементы с определенным классом
         elements_with_class_names = soup.find_all(class_="fh-5 fw-700 text-black")
         elements_with_class_prices = soup.find_all(class_="fh-4 fw-800 text-white")
-
+        all_elements = []
+        for element in elements_with_class_names:
+            all_elements.append(element.text)
+            for child in elements_with_class_prices:
+                all_elements.append(child.text)
+                break
         # Возвращаем текстовое содержимое найденных элементов
-        print([element.text for element in elements_with_class_prices])
-        return [element.text for element in elements_with_class_names]
+
+        # print([element.text for element in elements_with_class_prices])
+        return [element for element in all_elements]
     else:
         print("Ошибка при получении страницы:", response.status_code)
         return None
